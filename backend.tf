d46903606c54929227af4b45668f1ac4a8eff375
chrisrojas89 <chris_rojas89@outlook.com>
2026-06-26 11:52:29 -0500

    update

--- backend.tf
+++ backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "azurerm" {
     resource_group_name  = "rg-terraform-lab"
-    storage_account_name = "sttfstatelabpac2026"
+    storage_account_name = "sttfstatelabpac"
     container_name       = "tfstate"
     key                  = "terraform.tfstate"
     use_azuread_auth     = true
